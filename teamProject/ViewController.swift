a5a1d1a52222ca54445ab17f0e599a23e47536fd
Emily Rainer <erainer@e114-m-03.mchenry.edu>
2018-09-05 13:34:40 -0500

    Initial Commit

---
+++ teamProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  teamProject
+//
+//  Created by Emily Rainer on 9/5/18.
+//  Copyright © 2018 Emily Rainer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
